49a722ec55c0f8ae7665b6e7b0426e45cf30b857
Wang Rong <wangron@tesla.com>
2022-11-23 19:27:32 +0800

    add producer function for Rabbit

--- main/main.go
+++ main/main.go
@@ -3,23 +3,49 @@ package main
 import (
 	"bytes"
 	"flag"
+	"fmt"
 	"log"
 	"time"
 
 	"github.com/streadway/amqp"
 )
 
-var queue string
+var (
+	queue      string
+	exchange   string
+	routingKey string
+)
 
+// producer job
 func init() {
-	flag.StringVar(&queue, "qs", "info_queue", "provide your queue name")
+	flag.StringVar(&exchange, "ex", "logs_topic", "prodide your exchange name")
+	flag.StringVar(&routingKey, "rk", "test", "provide your routing key")
 }
-
 func main() {
 	flag.Parse()
 	rabbit := NewRabbit("amqp://admin:admin@localhost:5672/test")
-	rabbit.Consume(queue, doWork)
+	var input string
+	for {
+		fmt.Println("input your send message")
+		fmt.Scanf("%s", &input)
+		if err := rabbit.Producer(exchange, routingKey, input); err != nil {
+			fmt.Println("produce message error ", err)
+			continue
+		}
+		fmt.Println("produce message success")
+	}
 }
+
+// worker job
+// func init() {
+// 	flag.StringVar(&queue, "qs", "info_queue", "provide your queue name")
+// }
+
+// func main() {
+// 	flag.Parse()
+// 	rabbit := NewRabbit("amqp://admin:admin@localhost:5672/test")
+// 	rabbit.Consume(queue, doWork)
+// }
 
 func doWork(msg amqp.Delivery) error {
 	log.Printf("get message: %s", msg.Body)
--- main/worker.go
+++ main/worker.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"log"
 	"time"
@@ -46,6 +47,9 @@ func (r *Rabbit) getChannel() error {
 		if err := r.connect(); err != nil {
 			return err
 		}
+	}
+	if r.channel != nil {
+		return nil
 	}
 	log.Println("get channel: begin get")
 	channel, err := r.connection.Channel()
@@ -129,4 +133,55 @@ func (r *Rabbit) Consume(queueName string, fn func(amqp.Delivery) error) error {
 		}
 		r.consumeMsg(queueName, fn)
 	}
+}
+
+func (r *Rabbit) Producer(exchangeName, routingKey string, msg interface{}) error {
+	for i := 0; i < 30; i++ {
+		if err := r.connect(); err != nil {
+			log.Printf("%dth reconnect failed, %s\n", i, err)
+			time.Sleep(time.Second)
+			continue
+		}
+		if err := r.getChannel(); err != nil {
+			log.Printf("%dth get channel failed, %s\n", i, err)
+			continue
+		}
+		if err := r.channel.ExchangeDeclare(
+			exchangeName, // name
+			"topic",      // type
+			true,         // durable
+			false,        // auto-deleted
+			false,        // internal
+			false,        // noWait
+			nil,          // arguments
+		); err != nil {
+			log.Println("declear exchange error: ", err)
+			return err
+		}
+		body, err := json.Marshal(msg)
+		if err != nil {
+			log.Println("marshal msg error", err)
+			return err
+		}
+		if err := r.channel.Publish(
+			exchangeName, // exchange
+			routingKey,   // routing key
+			false,        // mandatory
+			false,        // immediate
+			amqp.Publishing{
+				DeliveryMode: amqp.Persistent,
+				ContentType:  "application/json",
+				Body:         body,
+			},
+		); err != nil {
+			log.Println("publish message error: ", err, ", will reconnect again")
+			r.connection = nil
+			r.channel = nil
+			continue
+		}
+		return nil
+	}
+	// all retry failed
+	log.Println("all retry failed, please check error log")
+	return errors.New("publish message failed")
 }
